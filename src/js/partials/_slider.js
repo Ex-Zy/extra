efebacf1eec6b2d2a9ebfa620d17536cfaadc250
Sergey <s.nagornykh@tekrum.pro>
2017-09-05 01:57:01 +0300

    add product slider

--- src/js/partials/_slider.js
+++ src/js/partials/_slider.js
@@ -2,6 +2,8 @@ import '../lib/slick.min';
 
 const SLIDER_EXTRA = $('.js-slider-extra');
 const SLIDER_ADVANAGES = $('.js-slider-advantages');
+const SLIDER_PRODUCT = $('.js-slider-product');
+const SLIDER_THUMBS = $('.js-slider-thumbs');
 
 function initSlider(slider, options) {
 	slider.not('.slick-initialized').slick(options);
@@ -21,4 +23,18 @@ initSlider(SLIDER_ADVANAGES, {
 	speed: 800,
 	autoplaySpeed: 4000,
 	dots: true
+});
+
+initSlider(SLIDER_PRODUCT, {
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: SLIDER_THUMBS
+});
+
+initSlider(SLIDER_THUMBS, {
+	slidesToShow: 5,
+	vertical: true,
+	asNavFor: SLIDER_PRODUCT
 });
